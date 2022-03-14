802bc3a16db7dd9a61421d6c763148d88fafb0d1
Devik Gupta <64042688+guptadevik2003@users.noreply.github.com>
2022-03-15 01:51:57 +0530

    added api route

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,4 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
+// /api/
+router.get('/', async (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: 'API Route Working!',
+        timestamp: Date.now()
+    })
+})
+
 module.exports = router
