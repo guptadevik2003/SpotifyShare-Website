8774c24618a29d2cef2eb4f1e14f9bedcef66353
Devik Gupta <64042688+guptadevik2003@users.noreply.github.com>
2022-04-28 03:51:52 +0530

    Update home.js

--- views/javascripts/home.js
+++ views/javascripts/home.js
@@ -1,8 +1,18 @@
 console.log('Welcome to Spotify Share.')
 
+const modal = document.getElementById('login-modal')
+const passwdInput = document.getElementById('password')
+let userPassword = ''
+
+passwdInput.addEventListener('keyup', async (event) => {
+    if (event.keyCode === 13) {
+        event.preventDefault()
+        loginBtnClicked()
+    }
+})
+
 const loginBtnClicked = async () => {
-    const modal = document.getElementById('login-modal')
-    const userPassword = document.getElementById('password').value
+    userPassword = passwdInput.value
     const checkUserPasswordURL = `https://spotifyshare.mapleclub.top/api/check-user-password?user_password=${userPassword}`
     await fetch(checkUserPasswordURL)
     .then(res => res.json())
